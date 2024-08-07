44273c9158fc7a8010b4d121c54798a2ebb320a4
kangddong <rkdehddud96@naver.com>
2024-08-07 20:50:01 +0900

    MapView 추가

---
+++ LandmarksApp/arex/LandmarksApp/LandmarksApp/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  LandmarksApp
+//
+//  Created by 강동영 on 8/7/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
