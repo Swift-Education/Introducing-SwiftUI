8f93e77337232e9c0f18720c504024443fd36ac6
kangddong <rkdehddud96@naver.com>
2024-08-07 16:14:09 +0900

    add project

---
+++ CreatingAndCombingViews/final/CreatingAndCombingViews/CreatingAndCombingViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAndCombingViewsApp.swift
+//  CreatingAndCombingViews
+//
+//  Created by 강동영 on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAndCombingViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CreatingAndCombingViews/starter/CreatingAndCombingViews/CreatingAndCombingViews/CreatingAndCombingViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAndCombingViewsApp.swift
+//  CreatingAndCombingViews
+//
+//  Created by 강동영 on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAndCombingViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
