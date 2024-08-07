2b05d5daf2c00d9f94943bcdd870729dbf69f2f2
kangddong <rkdehddud96@naver.com>
2024-08-07 20:48:33 +0900

    Section 4 done

--- LandmarksApp/arex/LandmarksApp/LandmarksApp/CircleImage.swift
+++ LandmarksApp/arex/LandmarksApp/LandmarksApp/CircleImage.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Image(.turtlerock)
+                .clipShape(Circle())
+                .shadow(radius: 7)
+            Circle().stroke(.gray, lineWidth: 4)
+        }
+        
+        Image(.turtlerock)
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 
