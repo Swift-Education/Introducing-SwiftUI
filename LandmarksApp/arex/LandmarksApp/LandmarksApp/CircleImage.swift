e438acc8fde0b69dcd486e4d98ae6c098f542ddd
kangddong <rkdehddud96@naver.com>
2024-08-07 20:37:57 +0900

    add CircleImage View

---
+++ LandmarksApp/arex/LandmarksApp/LandmarksApp/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  LandmarksApp
+//
+//  Created by 강동영 on 8/7/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
