610986bcc901abfe740fd6b5961dc2be72996ba7
kangddong <rkdehddud96@naver.com>
2025-02-07 23:36:41 +0900

    re initial commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 강동영 on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
